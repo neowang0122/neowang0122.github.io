2c35233d4ac7d809ce76a35e8d0a24a10ae833de
neowang <neowang0122@gmail.com>
2024-03-20 08:18:52 -0400

    Force Mobile layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,6 +16,9 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    // Component.PageTitle(),
+    // Component.Search(),
+    // Component.Darkmode(),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
@@ -24,12 +27,16 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   left: [
     Component.PageTitle(),
+    Component.Darkmode(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Darkmode(),
+    // Component.PageTitle(),
+    // Component.MobileOnly(Component.Spacer()),
+    // Component.Search(),
+    // Component.Darkmode(),
     // Component.DesktopOnly(Component.RecentNotes()), 
     // Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.TableOfContents()),
+    // Component.DesktopOnly(Component.TableOfContents()),
   ],
   right: [
     // Component.DesktopOnly(Component.Graph()),
@@ -41,15 +48,19 @@ export const defaultContentPageLayout: PageLayout = {
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [
+    // Component.PageTitle(),
+    // Component.Search(),
+    // Component.Darkmode(),
+    // 
     Component.Breadcrumbs(),
     Component.ArticleTitle(), 
     Component.ContentMeta(), 
   ],
   left: [
     Component.PageTitle(),
+    Component.Darkmode(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Darkmode(),
     // Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
