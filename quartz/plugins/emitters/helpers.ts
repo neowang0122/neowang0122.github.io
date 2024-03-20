ecfb12a9dca2c5160d21383364a4d2df30f1990a
neowang <neowang0122@gmail.com>
2024-03-20 02:17:01 -0400

    Automatically create index.md for each folder to avoid 404 error

--- quartz/plugins/emitters/helpers.ts
+++ quartz/plugins/emitters/helpers.ts
@@ -17,3 +17,52 @@ export const write = async ({ ctx, slug, ext, content }: WriteOptions): Promise<
   await fs.promises.writeFile(pathToPage, content)
   return pathToPage
 }
+
+
+// Automatically create index.md for each folder to avoid 404 error
+export const createIndexMd = async () => {  
+  const contentItems = await fs.promises.readdir("content", {  
+    encoding: "utf-8",  
+    recursive: true,  
+    withFileTypes: true,  
+  })  
+  
+  // index.md  
+  for (let item of contentItems) {  
+    try {  
+      if (!item.isFile() && item.name !== "image") {  
+        const dir = path.join(item.path, item.name, "index.md")  
+  
+        // Tag processing: 1st stage - folder name, 2nd stage - parent folder name
+        let tag: string[] = []  
+  
+        // 1 stage
+        let current = filterTag(item.name)  
+        tag.push(current)  
+  
+        // 2 stage
+        const parentItem = item.path.split("/")  
+        if (parentItem[parentItem.length - 1] !== "content") {  
+          let parent = filterTag(parentItem[parentItem.length - 1])  
+          tag.push(parent)  
+        }  
+  
+        await fs.promises.writeFile(dir, `---\ntitle: ${item.name}\ntag: ${tag}\n---`)  
+      }  
+    } catch (e) {  
+      console.log(e)  
+    }  }  
+}  
+  
+const filterTag = (item: string) => {  
+  let result: string[] = []  
+  
+  item.split("").map(r => {  
+    if (r !== "(" && r !== ")") {  
+      if (r == " ") result.push("-")  
+      else result.push(r)  
+    }  
+  })  
+  
+  return result.join('')  
+}
